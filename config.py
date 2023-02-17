47f51206bec1ee3b3d8c91ce4206969b3badfa7c
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-02-17 23:40:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5713245787:AAF-x7x7vbxcWO98HFbrkPI8FDIH24iw4Ww")
+APP_ID = int(os.environ.get("API_ID", "24531402"))
+API_HASH = os.environ.get("API_HASH", "0f75926e95a476ccff47dacb52079053")
 
 youtube_next_fetch = 0  # time in minute
 
